b019cbca0b2a7a003946a795c58cb01b2708848b
ZM-Omar <l21212374@tectijuana.edu.mx>
2025-01-08 11:56:33 -0800

    initial commit

---
+++ MauiApp1/GenerarSala.xaml.cs
@@ -0,0 +1,22 @@
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class GenerarSalaPage : ContentPage
+    {
+        public GenerarSalaPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenerateCodeClicked(object sender, EventArgs e)
+        {
+            // Lógica para generar el código QR y mostrarlo en QRCodeImage
+        }
+
+        private void OnSaveCodeClicked(object sender, EventArgs e)
+        {
+            // Lógica para guardar el código
+        }
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnGenerateSalaClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GenerarSalaPage());
+        }
+    }
+}
